059eb01c19ec4a6eecaf440f548adeb4992e2f97
JC949 <jacobcampbell180@yahoo.com>
2023-11-13 23:11:00 -0700

    Update Java Version

--- src/main/java/com/cove/CodeCoveApplication.java
+++ src/main/java/com/cove/CodeCoveApplication.java
@@ -2,8 +2,10 @@ package com.cove;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.cove")
 public class CodeCoveApplication {
 
 	public static void main(String[] args) {
